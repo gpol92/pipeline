ce903d6bc90ef9eaaeb350a84352eb0adbe1d02f
Gianmarco Polizia <gianmarco.polizia@keysight.com>
2025-01-22 17:13:58 +0100

    22/01/2025

--- ForwardingUnit.vhd
+++ ForwardingUnit.vhd
@@ -6,6 +6,7 @@ entity ForwardingUnit is
 	Port (
 		clk: in std_logic;
 		reset: in std_logic;
+		RegWrite: in std_logic;
 		ID_EX_RegAddr1: in std_logic_vector(4 downto 0);
 		ID_EX_RegAddr2: in std_logic_vector(4 downto 0);
 		EX_MEM_DestReg: in std_logic_vector(4 downto 0);
@@ -23,22 +24,25 @@ begin
 			forwardA <= "00";
 			forwardB <= "00";
 		elsif rising_edge(clk) then
-			-- Forwarding for A
-			if ID_EX_RegAddr1 = EX_MEM_DestReg and EX_MEM_DestReg /= "00000" then
-				forwardA <= "10";
-			elsif ID_EX_RegAddr1 = MEM_WB_DestReg and MEM_WB_DestReg /= "00000" then
-				forwardA <= "01";
-			else
-				forwardA <= "00";
-			end if;
+			
+			if RegWrite = '1' then
+				-- Forwarding for A
+				if ID_EX_RegAddr1 = EX_MEM_DestReg and EX_MEM_DestReg /= "00000" then
+					forwardA <= "10";
+				elsif ID_EX_RegAddr1 = MEM_WB_DestReg and MEM_WB_DestReg /= "00000" then
+					forwardA <= "01";
+				else
+					forwardA <= "00";
+				end if;
 
-			-- Forwarding for B
-			if ID_EX_RegAddr2 = EX_MEM_DestReg and EX_MEM_DestReg /= "00000" then
-				forwardB <= "10";
-			elsif ID_EX_RegAddr2 = MEM_WB_DestReg and MEM_WB_DestReg /= "00000" then
-				forwardB <= "01";
-			else
-				forwardB <= "00";
+				-- Forwarding for B
+				if ID_EX_RegAddr2 = EX_MEM_DestReg and EX_MEM_DestReg /= "00000" then
+					forwardB <= "10";
+				elsif ID_EX_RegAddr2 = MEM_WB_DestReg and MEM_WB_DestReg /= "00000" then
+					forwardB <= "01";
+				else
+					forwardB <= "00";
+				end if;
 			end if;
 		end if;
 	end process;
